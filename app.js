c33d72805922df0d0543405d155798925b540930
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-03 11:58:26 +0100

    Create basic server game lobby functionality

--- app.js
+++ app.js
@@ -1,61 +1,10 @@
 const express = require('express');
-const WebSocket = require('ws');
-const { WebSocketServer } = require('ws');
-require('dotenv').config();
+const gameServer = require('./gameServer');
 
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 // public/index.html will automatically be sent to the http client.
 app.use(express.static('public'));
-
-const clientMetadata = new Map();
-const messages = [];
-
-// app.listen() returns a nodejs httpServer, which wss can piggyback on the same port.
-const wss = new WebSocketServer({ server: app.listen(PORT) });
-wss.on('connection', (ws) => {
-  ws.on('error', console.error);
-
-  ws.on('message', (data) => {
-    const json = JSON.parse(data);
-
-    switch (json.type) {
-      case 'opened': {
-        const { uuid } = json;
-        if (!clientMetadata.has(ws)) {
-          clientMetadata.set(ws, { id: uuid, time_connected: Date.now() });
-          console.log('Client connected via websockets: ', uuid);
-        }
-        break;
-      }
-
-      case 'closed': {
-        const { uuid } = json;
-        if (clientMetadata.has(ws)) {
-          clientMetadata.delete(ws);
-          console.log('Client disconnected via websockets: ', uuid);
-        }
-        break;
-      }
-
-      case 'message_submitted': {
-        const { message } = json;
-        if (message !== '') {
-          messages.push(message);
-          wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(
-                JSON.stringify({ type: 'messages_updated', messages }),
-              );
-            }
-          });
-        }
-        break;
-      }
-    }
-  });
-
-  // Send initial batch of messages.
-  ws.send(JSON.stringify({ type: 'messages_updated', messages }));
-});
+const wss = gameServer(app, PORT);
---
+++ gameServer.js
@@ -0,0 +1,171 @@
+const WebSocket = require('ws');
+const { WebSocketServer } = require('ws');
+const Player = require('./game/player');
+const Game = require('./game/game');
+
+const MAX_GAMES = process.env.MAX_GAMES || 4;
+
+// ws object maps to all other info, e.g. player object, lobby object
+const clientMetadata = new Map();
+// lobby_name maps to game object
+const games = new Map();
+
+// app.listen() returns a nodejs httpServer, which wss can piggyback on the same port.
+function gameServer(app, port) {
+  const wss = new WebSocketServer({ server: app.listen(port) });
+  wss.on('connection', (ws) => {
+    ws.on('error', console.error);
+
+    ws.on('message', (wsData) => {
+      const { type, ...data } = JSON.parse(wsData);
+
+      switch (type) {
+        case 'opened': {
+          const { uuid } = data;
+          if (!clientMetadata.has(ws)) {
+            clientMetadata.set(ws, {
+              id: uuid,
+              time_connected: Date.now(),
+              player: new Player(),
+              lobby: null,
+            });
+            console.log('Client connected via websockets: ', uuid);
+          }
+          break;
+        }
+
+        case 'closed': {
+          const { uuid } = data;
+          if (clientMetadata.has(ws)) {
+            clientMetadata.delete(ws);
+            console.log('Client disconnected via websockets: ', uuid);
+          }
+          break;
+        }
+
+        case 'new_lobby_requested': {
+          const { lobby_name } = data;
+          if (games.size >= MAX_GAMES) {
+            ws.send(
+              JSON.stringify({
+                type: 'message_received',
+                message: 'The maximum number of lobbies are already open.',
+              }),
+            );
+          } else if (games.has(lobby_name)) {
+            ws.send(
+              JSON.stringify({
+                type: 'message_received',
+                message: 'Lobby with that name already exists.',
+              }),
+            );
+          } else {
+            console.log(
+              `New lobby created by ${clientMetadata.get(ws).id}: ${lobby_name}`,
+            );
+            games.set(lobby_name, new Game(sendGameUpdate));
+            ws.send(
+              JSON.stringify({
+                type: 'message_received',
+                message: `New lobby was created: ${lobby_name}`,
+              }),
+            );
+            sendLobbyListUpdate(games);
+          }
+          break;
+        }
+
+        case 'player_join_lobby_request': {
+          const { lobby_name } = data;
+          if (games.has(lobby_name)) {
+            // If player is already in a lobby, remove them.
+            const { id, player, lobby: previousLobby } = clientMetadata.get(ws);
+            if (previousLobby) previousLobby.removePlayer(player);
+
+            // Now join the new lobby.
+            const lobby = games.get(lobby_name);
+            if (lobby.playerCanJoin(player)) {
+              clientMetadata.get(ws).lobby = lobby;
+              lobby.addPlayer(player);
+              console.log(`Player: ${id} joined game: ${lobby_name}`);
+              // Send message to client about their amazing success.
+              ws.send(JSON.stringify({ type: 'joined_lobby', lobby_name }));
+            } else {
+              console.log('Player could not join game');
+              // Send message to client about their hideous failure.
+            }
+          } else {
+            // Send message to client saying lobby not found.
+          }
+          break;
+        }
+
+        case 'player_leave_lobby_request': {
+          const { id, player, lobby } = clientMetadata.get(ws);
+          lobby.removePlayer(player);
+          console.log(`Player: ${id} left game`);
+        }
+
+        case 'player_ready_changed': {
+          clientMetadata.get(ws).player.ready = data.ready;
+          console.log('GAMES: ', games);
+          // Check if all players in that player's game are ready.
+          // Need to find a nice way to link each websocket with a player and a game.
+          // Get game and player from websocket clientMetadata, then get the rest of the players from??
+          // Finding a good way to relate all this info to each other in a flexible way seems difficult.
+          // Link it all back to the ws?
+          // ws -> player
+          // ws -> lobby
+          // how to do lobby -> players?
+          // or lobby -> ws clients?
+          // ORRRR ws -> lobby -> player list
+          break;
+        }
+
+        case 'player_direction_changed': {
+          const { direction } = data;
+          // Change direction of player's snake.
+          const { player } = clientMetadata.get(ws);
+          break;
+        }
+      }
+    });
+
+    // Send initial list of lobbies.
+    ws.send(
+      JSON.stringify({
+        type: 'lobby_list_updated',
+        // Convert map into array of keys.
+        lobbies: [...games.keys()],
+      }),
+    );
+  });
+
+  function sendToAllClients(data) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  function sendLobbyListUpdate(games) {
+    sendToAllClients({
+      type: 'lobby_list_updated',
+      // Convert map into array of keys.
+      lobbies: [...games.keys()],
+    });
+  }
+
+  function sendGameUpdate(game) {
+    // In the future, send only to clients associated with this particular game, not ALL of them.
+    sendToAllClients({
+      type: 'game_updated',
+      game_state: game.packageGameData(),
+    });
+  }
+
+  return wss;
+}
+
+module.exports = gameServer;
