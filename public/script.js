003605be631ee52971255f3f6590375b76030a74
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-03 11:57:17 +0100

    Create a basic client that can create and join a game lobby

--- public/script.js
+++ public/script.js
@@ -2,7 +2,9 @@ const socket = new WebSocket('ws://localhost:8080');
 
 const uuid = self.crypto.randomUUID();
 
-let messagesElement = document.getElementById('messages');
+let currentLobbyElement = document.getElementById('current-lobby');
+let lobbyListElement = document.getElementById('lobby-list');
+const messagesElement = document.getElementById('messages');
 
 socket.onopen = () => {
   socket.send(JSON.stringify({ type: 'opened', uuid }));
@@ -12,34 +14,136 @@ window.onbeforeunload = () => {
   socket.send(JSON.stringify({ type: 'closed', uuid }));
 };
 
+addEventListener('keydown', (event) => {
+  const { key } = event;
+  switch (key) {
+    case 'w': {
+      socket.send(
+        JSON.stringify({ type: 'player_direction_changed', direction: 'up' }),
+      );
+      break;
+    }
+
+    case 'a': {
+      socket.send(
+        JSON.stringify({ type: 'player_direction_changed', direction: 'left' }),
+      );
+      break;
+    }
+
+    case 's': {
+      socket.send(
+        JSON.stringify({ type: 'player_direction_changed', direction: 'down' }),
+      );
+      break;
+    }
+
+    case 'd': {
+      socket.send(
+        JSON.stringify({
+          type: 'player_direction_changed',
+          direction: 'right',
+        }),
+      );
+      break;
+    }
+
+    case 'r': {
+      socket.send(
+        JSON.stringify({
+          type: 'player_ready_toggled',
+        }),
+      );
+    }
+  }
+});
+
 socket.onmessage = (event) => {
   const json = JSON.parse(event.data);
   switch (json.type) {
-    case 'messages_updated': {
-      console.log('Received message updates!');
-      messagesElement.remove();
-      messagesElement = createMessagesList(json.messages);
-      document.body.appendChild(messagesElement);
+    case 'lobby_list_updated': {
+      refreshLobbyListItems(lobbyListElement, json.lobbies);
+      break;
+    }
+
+    case 'joined_lobby': {
+      currentLobbyElement.innerText = `You're in: ${json.lobby_name}`;
+      break;
+    }
+
+    case 'left_lobby': {
+      currentLobbyElement.innerText = '';
+      break;
+    }
+
+    case 'game_updated': {
+      console.log('Game state updated: ', json.game_state);
+      // Draw game on screen or whatever.
+      break;
+    }
+
+    case 'message_received': {
+      messagesElement.appendChild(
+        createMessage(
+          `${new Date(Date.now()).toLocaleTimeString()} - ${json.message}`,
+        ),
+      );
       break;
     }
   }
 };
 
-function createMessagesList(messages) {
-  const ul = document.createElement('ul');
-  ul.id = 'messages';
-  for (const message of messages) {
-    const li = document.createElement('li');
-    li.innerText = message;
-    ul.appendChild(li);
-  }
-  return ul;
+function createMessage(message) {
+  const li = document.createElement('li');
+  li.innerText = message;
+  return li;
 }
 
-function sendMessage(event) {
+function createCurrentLobbyHeader(currentLobby) {
+  const h2 = document.createElement('h2');
+  h2.id = 'current-lobby';
+  h2.innerText = currentLobby;
+  return h2;
+}
+
+function refreshLobbyListItems(element, lobbies) {
+  // Remove any existing items.
+  const existingListItems = document.querySelectorAll('#lobby-list > li');
+  for (const existingListItem of existingListItems) {
+    existingListItem.remove();
+  }
+
+  // Now make the new ones.
+  for (const lobby of lobbies) {
+    const li = document.createElement('li');
+    li.innerText = lobby;
+    li.onclick = () => joinLobby(lobby);
+    element.appendChild(li);
+  }
+}
+
+function requestNewLobby(event) {
   event.preventDefault();
-  const { message } = Object.fromEntries(
+  const { lobby_name } = Object.fromEntries(
     new FormData(event.currentTarget).entries(),
   );
-  socket.send(JSON.stringify({ type: 'message_submitted', message }));
+  socket.send(JSON.stringify({ type: 'new_lobby_requested', lobby_name }));
+}
+
+function joinLobby(lobby_name) {
+  socket.send(
+    JSON.stringify({ type: 'player_join_lobby_request', lobby_name }),
+  );
+}
+
+function leaveLobby(lobby) {
+  socket.send(JSON.stringify({ type: 'player_leave_lobby_request' }));
+}
+
+function playerReady() {
+  socket.send(JSON.stringify({ type: 'player_ready_changed', ready: true }));
+}
+
+function playerNotReady() {
+  socket.send(JSON.stringify({ type: 'player_ready_changed', ready: false }));
 }
