f42cb0d197fecb0562357d73705ea06bd59024c8
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-16 12:47:20 +0100

    Switch from pure websockets to socket.io library

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const http = require('http');
+const dotenv = require('dotenv');
 const gameServer = require('./gameServer');
 
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+const PORT = process.env.VITE_SERVER_PORT || 3000;
 
 const app = express();
 // public/index.html will automatically be sent to the http client.
 app.use(express.static('public'));
-const wss = gameServer(app, PORT);
+const server = http.createServer(app);
+gameServer(server);
+server.listen(PORT);
